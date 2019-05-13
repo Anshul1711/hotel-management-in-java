6f0375d622356235fe0f0720c310bd11f2b28a6e
Denver995 <denverclaude99@gmail.com>
2019-05-13 20:28:28 +0200

    feat: Edditing Accueil.xml and Login.xml
    feat:Creating new ui to manage Client and Employe, Chambre and Salle classes

---
+++ src/fr/hotel/view/ClientMapping.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package fr.hotel.view;
+
+/**
+ * @author Denver99
+ *
+ */
+public class ClientMapping {
+
+	/**
+	 * 
+	 */
+	public ClientMapping() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
