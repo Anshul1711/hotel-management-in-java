419bfbf1b40cef557324d9759822aab7f75daa65
Denver995 <denverclaude99@gmail.com>
2019-05-13 16:12:40 +0200

    feat: Creating an Crud interface that will be implement by each classes

---
+++ src/fr/hotel/model/Model1/CrudInterface.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.hotel.model.Model1;
+
+/**
+ * @author Denver99
+ *
+ */
+public interface CrudInterface {
+	
+	   public void ajouter();
+	   
+	   public void modifier();
+
+	   public void supprimer();
+
+	   public void afficher();
+
+}
