e38ee1f676e1f7658c10edc84c275d586aa0a362
Denver995 <denverclaude99@gmail.com>
2019-05-14 16:57:54 +0200

    feat: Editing Accueil.fxml file and adding new functionalities in ClientMapping class to manage client

--- src/fr/hotel/view/ClientMapping.java
+++ src/fr/hotel/view/ClientMapping.java
@@ -6,6 +6,8 @@ package fr.hotel.view;
 import fr.hotel.MainClass;
 import fr.hotel.model.Model1.Client;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -58,6 +60,8 @@ public class ClientMapping {
         // Initialize the Client table with the two columns.
         nomColumn.setCellValueFactory(cellData -> cellData.getValue().getNom());
         prenomColumn.setCellValueFactory(cellData -> cellData.getValue().getPrenom());
+        //adding a listerner to the clientTable
+        clientTable.getSelectionModel().selectedItemProperty().addListener((observable, oldvalue, newvalue) -> afficher(newvalue));
     }
 
     //Méthode qui sera utilisée dans l'initialisation de l'IHM
@@ -66,5 +70,37 @@ public class ClientMapping {
         this.main = mainApp;
         // On lie notre liste observable au composant TableView
         this.clientTable.setItems(main.getListClient());
+    }
+    
+    //Function that will used to show a selected client in the ui
+    @FXML
+    public void afficher(Client c) {
+    	
+    	nomValue.setText("");
+    	prenomValue.setText("");
+    	numCniValue.setText("");
+    	sexeValue.setText("");
+    	nationaliteValue.setText("");
+    	if(c!=null) {
+    		nomValue.setText(c.getNom().get());
+        	prenomValue.setText(c.getPrenom().get());
+        	numCniValue.setText(c.getNumCni().get());
+        	sexeValue.setText(c.getSexe().toString());
+        	nationaliteValue.setText(c.getNationnalite().get());
+    	}
+    }
+    
+    //function that will remove a selected client to the ui
+    @FXML
+    public void supprimer() {
+    	int index = clientTable.getSelectionModel().getSelectedIndex();
+    	if(index > 1)
+    		clientTable.getItems().remove(index);
+    	else {
+    		Alert problem = new Alert(AlertType.ERROR);
+    		problem.setTitle("Erreur");
+    		problem.setHeaderText("Vous devez selectionner l'élément à supprimer");
+    		problem.showAndWait();
+    	}
     }
 }
